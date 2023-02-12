d31fd136dab1298f1340c97c941919eb7b2d29ef
hong-kyungs <hkyungs11@naver.com>
2023-02-12 23:53:50 +0900

    4. 야구게임 - require가 아님 import 사용

--- 3. 끝말잇기/client.jsx
+++ 3. 끝말잇기/client.jsx
@@ -1,6 +1,6 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const WordRelay = require('./WordRelay');
+import NumberBaseball from './NumberBaseball';
 
-ReactDOM.createRoot(document.querySelector('#root')).render(<WordRelay />);
+ReactDOM.createRoot(document.querySelector('#root')).render(<NumberBaseball />);
---
+++ 4. 야구게임/NumberBaseball.jsx
@@ -0,0 +1,8 @@
+import React, { Component }from 'react';
+// import { Component } from 'react'; // 위처럼 합쳐줄 수 있다.
+
+class NumberBaseball extends Component {
+
+}
+
+export default NumberBaseball;
