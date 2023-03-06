a06f0a2d355339104f0bd5b10dea5ee161ad92a4
hong-kyungs <hkyungs11@naver.com>
2023-03-06 17:13:29 +0900

    useContext 사용해 지뢰칸 렌더링하기

--- 9. 지뢰찾기/MineSearch.jsx
+++ 9. 지뢰찾기/MineSearch.jsx
@@ -10,7 +10,7 @@ export const CODE = {
   QUESTION_MINE: -4,
   FLAG_MINE: -5,
   CLICKED_MINE: -6,
-  OPENED: 0,
+  OPENED: 0, //0이상 이면 다 opened
 }
 
 export const TableContext = createContext({
@@ -25,7 +25,36 @@ const initialState = {
   result: '',
 };
 
-const START_GAME = 'START_GAME';
+const plantMine = (row, cell, mine) => {
+  console.log(row, cell, mine);
+  const candidate = Array(row * cell).fill().map((arr, i) => {
+    return i;
+  })
+  const shuffle = [];
+  while(candidate.length > row * cell - mine){
+    const chosen = candidate.splice(Math.floor(Math.random() * candidate.length) ,1)[0];
+    shuffle.push(chosen);
+  }
+  const data = [];
+  for(let i = 0; i < row; i++){
+    const rowData = [];
+    data.push(rowData);
+    for(let j = 0; j < cell; j++){
+      rowData.push(CODE.NORMAL);
+    }
+  }
+
+  for(let k = 0; k < shuffle.length; k++) {
+    const ver = Math.floor(shuffle[k] / cell);
+    const hor = shuffle[k] % cell;
+    data[ver][hor] = CODE.MINE;
+  }
+
+  console.log(data);
+  return data;
+}
+
+export const START_GAME = 'START_GAME';
 
 const reducer = (state, action) => {
   switch(action.type){
