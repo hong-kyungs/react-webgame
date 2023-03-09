9b1957f767b17b9807f3cbd850087af23508f87d
hong-kyungs <hkyungs11@naver.com>
2023-03-09 17:16:01 +0900

    Dinamic Route Match(동적 라우트 매칭)

---
+++ react-router/GameMatcher.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import NumberBaseball from '../4. 야구게임/NumberBaseballClass';
+import RSP from '../6. 가위바위보/RSPClass';
+import Lotto from '../7. 로또추첨기/LottoClass';
+
+class GameMatcher extends Component {
+render() {
+    console.log(this.props);
+    if(this.props.match.params.name === 'number-baseball'){
+      return <NumberBaseball />
+    } else if(this.props.match.params.name === 'rock-scissors-paper'){
+      return <RSP />
+    } else if(this.props.match.params.name === 'lotto-generator'){
+      return <Lotto />
+    }
+    return(
+      <div>
+        일치하는 게임이 없습니다.
+      </div>
+    );
+  }
+}
+export default GameMatcher;
--- react-router/Games.jsx
+++ react-router/Games.jsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
-import NumberBaseball from '../4. 야구게임/NumberBaseballClass';
-import RSP from '../6. 가위바위보/RSPClass';
-import Lotto from '../7. 로또추첨기/LottoClass';
+// import NumberBaseball from '../4. 야구게임/NumberBaseballClass';
+// import RSP from '../6. 가위바위보/RSPClass';
+// import Lotto from '../7. 로또추첨기/LottoClass';
+import GameMatcher from './GameMatcher';
 
 const Games = () => {
   return(
     <BrowserRouter>
       <div>
         공통된 부분
-        <Link to="/number-baseball">숫자야구</Link>
+        <Link to="/game/number-baseball">숫자야구</Link>
         &nbsp;
-        <Link to="/rock-scissors-paper">가위바위보</Link>
+        <Link to="/game/rock-scissors-paper">가위바위보</Link>
         &nbsp;
-        <Link to="/lotto-generator">로또생성기</Link>
+        <Link to="/game/lotto-generator">로또생성기</Link>
+        &nbsp;
+        <Link to="/game/index">게임매쳐</Link>
       </div>
       <div>
+          {/* 
           <Route path="/number-baseball" component={NumberBaseball}/>
           <Route path="/rock-scissors-paper" component={RSP}/>
-          <Route path="/lotto-generator" component={Lotto}/>
+          <Route path="/lotto-generator" component={Lotto}/> 
+          */}
+          <Route path="/game/:name" component={GameMatcher}/>
       </div>
     </BrowserRouter>
   );
